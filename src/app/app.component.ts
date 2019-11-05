05149cd7914ff05f799bcb3ecd7b6cc41f596b01
michaelmccarty <mccartyatl@gmail.com>
2019-11-04 23:26:03 -0500

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-mew',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
