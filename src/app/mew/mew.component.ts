1578cc39f8489fc2cac6b911954ef9a1a1a4502f
michaelmccarty <mccartyatl@gmail.com>
2019-11-04 20:50:49 -0500

    Deleted tests, added Mew component. It is mysterious.

---
+++ src/app/mew/mew.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mew',
+  templateUrl: './mew.component.html',
+  styleUrls: ['./mew.component.css']
+})
+export class MewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
