598ff7429c2eda96abb5fdf544d723b6f7e70ff8
Michael McCarty <mmccarty@sgintl.com>
2019-11-05 15:04:30 -0500

    Mew works!

--- src/app/mew/mew.component.ts
+++ src/app/mew/mew.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-mew',
-  templateUrl: './mew.component.html',
-  styleUrls: ['./mew.component.css']
+  templateUrl: './mew.component.html'
 })
 export class MewComponent implements OnInit {
 
