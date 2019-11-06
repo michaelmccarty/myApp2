16f9c3be2304acc4a4fba1fd38235d1198dd8d37
Michael McCarty <mmccarty@sgintl.com>
2019-11-06 17:54:41 -0500

    Turned on CORS. Front and Back linked.

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ let PORT = process.env.PORT || 3000;
 // Initialize Express
 let app = express();
 
+app.use((req,res,next)=>{
+res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader(
+  "Access-Control-Allow-Headers",
+  "Origin, X-Requested-With, Content-Type, Accept"
+);
+res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+next();
+});
+
 // Configure middleware
 
 // Use morgan logger for logging requests
