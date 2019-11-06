d5ca6ad399a5e3d2500a09f85fcad067f600efaa
Michael McCarty <mmccarty@sgintl.com>
2019-11-06 11:28:43 -0500

    Refinement

--- apiRoutes.js
+++ apiRoutes.js
@@ -1,16 +1,10 @@
-let axios = require("axios");
-let path = require("path");
-
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
 module.exports = function(app, db) {
-  app.post("/login", function(req, res) {
-    let username = req.body.username;
-    let password = req.body.password;
-
+  app.post("/go", function(req, res) {
     
-    console.log(req.body.username);
+    console.log(req.body);
   });
 };
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MewComponent } from './mew/mew.component';
 import { Mew2Component } from './mew2/mew2.component';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { Mew2Component } from './mew2/mew2.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
