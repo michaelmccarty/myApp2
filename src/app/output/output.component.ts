fdb41731230e735c02e634febf6766fcb03b9009
Michael McCarty <mmccarty@sgintl.com>
2019-11-07 14:49:08 -0500

    Fixed minor template bug

--- src/app/output/output.component.ts
+++ src/app/output/output.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-output',
   templateUrl: './output.component.html'
--- src/app/state/state.component.ts
+++ src/app/state/state.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-state'
+  selector: 'app-state',
+  templateUrl: './state.component.html'
 })
 export class StateComponent implements OnInit {
 
