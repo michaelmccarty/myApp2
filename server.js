addd90cb90e83b622c6b1ce620f59433fce84807
Michael McCarty <mmccarty@sgintl.com>
2019-11-07 10:49:07 -0500

    "npm start" script written.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 let express = require("express");
 let logger = require("morgan");
 
+const HTTP_CONTROL_METHODS = "GET, POST, OPTIONS";
+
 
 let PORT = process.env.PORT || 3000;
 
@@ -13,7 +15,7 @@ app.use((req,res,next)=>{   //ENABLE CORS
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", HTTP_CONTROL_METHODS);
   next();
 });
 
