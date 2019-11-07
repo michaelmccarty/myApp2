c1d1ceaca24672f5a2fa1f8c6c97e9c5bf167fe2
Michael McCarty <mmccarty@sgintl.com>
2019-11-07 14:35:57 -0500

    added output, broke code

---
+++ src/app/output/output.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-output',
+  templateUrl: './output.component.html'
+})
+export class OutputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/state/state.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-state'
+})
+export class StateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
