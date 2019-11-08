0b4d647571a9e67b9d3e1b64b1a4728145130e18
Michael McCarty <mmccarty@sgintl.com>
2019-11-08 10:07:45 -0500

    Added state.service.ts and removed StateComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { InputComponent } from './input/input.component';
 import { SubmitComponent } from './submit/submit.component';
-import { StateComponent } from './state/state.component';
 import { OutputComponent } from './output/output.component';
 
 
@@ -15,13 +15,13 @@ import { OutputComponent } from './output/output.component';
     AppComponent,
     InputComponent,
     SubmitComponent,
-    StateComponent,
     OutputComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputComponent implements OnInit {
 
+  userInput = new FormControl('');
   constructor() {
 
   }
