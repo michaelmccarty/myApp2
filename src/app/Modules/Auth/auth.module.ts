6831f610074226f852c0d8410eedafe5f0c97f24
Michael McCarty <mmccarty@sgintl.com>
2019-11-13 11:29:14 -0500

    Fixed module system. Works!

--- src/app/Modules/Auth/auth.module.ts
+++ src/app/Modules/Auth/auth.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+
 import { LoginComponent } from '../Auth/login/login.component';
 
+import { AuthService } from './auth.service';
+ 
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent],
   imports: [
     CommonModule,
     HttpClientModule
   ],
-  bootstrap: [LoginComponent]
+  bootstrap: [LoginComponent],
+  providers: [AuthService],
+  exports: [LoginComponent]
 })
 export class AuthModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { AuthModule } from './Modules/Auth/auth.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
