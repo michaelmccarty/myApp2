a4a787bde1c885016e68a276185d1628289c90b0
Michael McCarty <mmccarty@sgintl.com>
2019-11-13 11:48:20 -0500

    Added Main module with dash

---
+++ src/app/Modules/main/main.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashComponent } from './dash/dash.component';
+
+
+
+@NgModule({
+  declarations: [DashComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class MainModule { }
