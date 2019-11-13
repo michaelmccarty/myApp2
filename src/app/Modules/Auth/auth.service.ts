781657511b1585074cd39874f1d47d50b8c807fd
Michael McCarty <mmccarty@sgintl.com>
2019-11-13 10:25:32 -0500

    Refactoring naming

---
+++ src/app/Modules/Auth/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  dataStore: string[];
+  constructor(private http: HttpClient) { };
+
+  getData() {
+
+    return this.http.get("http://localhost:3000/api");
+
+  }
+
+  postData(toPost:string) {
+
+    return this.http.post("http://localhost:3000/api", {"toPost": toPost});
+
+  }
+
+
+  clearData() {
+    this.dataStore = [];
+  }
+
+
+}
