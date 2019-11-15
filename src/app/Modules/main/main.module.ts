4fe9fe199f1b4718fea63bc6ff0eddd4b43f2219
Michael McCarty <mmccarty@sgintl.com>
2019-11-15 17:53:44 -0500

    Changed form component to be more semantic

--- src/app/Modules/main/main.module.ts
+++ src/app/Modules/main/main.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashComponent } from './dash/dash.component';
-import { FormComponent } from './form/form.component';
 import { RouterModule } from '@angular/router';
 import { UnitComponent } from './unit/unit.component';
 import { UnitsComponent } from './units/units.component';
+import { DataFormComponent } from './data-form/data-form.component';
 
 
 @NgModule({
-  declarations: [DashComponent, FormComponent, UnitComponent, UnitsComponent],
+  declarations: [DashComponent, UnitComponent, UnitsComponent, DataFormComponent],
   imports: [
     CommonModule,
     RouterModule
