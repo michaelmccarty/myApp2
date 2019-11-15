b1ac11dafc19620b6c848c04c7b6704c0810e18e
Michael McCarty <mmccarty@sgintl.com>
2019-11-15 10:49:52 -0500

    Auth and Dash linked with router.

--- src/app/Modules/main/main.module.ts
+++ src/app/Modules/main/main.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashComponent } from './dash/dash.component';
 import { FormComponent } from './form/form.component';
-
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [DashComponent, FormComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [DashComponent]
 })
