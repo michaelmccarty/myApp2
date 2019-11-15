9929d8b5af8144d67b11da6b40c4563beb0425af
Michael McCarty <mmccarty@sgintl.com>
2019-11-15 18:24:35 -0500

    Done for day, for real.

--- src/app/Modules/main/unit/unit.component.ts
+++ src/app/Modules/main/unit/unit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-unit',
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UnitComponent implements OnInit {
 
-  value: string;
-  
+  @Input() unitName: string;
+
   constructor() { }
 
   ngOnInit() {
