3ca73a0302d5a429880c56a431b166db68a92eb8
Michael McCarty <mmccarty@sgintl.com>
2019-11-15 10:44:56 -0500

    Removed css from template

--- src/app/Modules/main/form/form.component.ts
+++ src/app/Modules/main/form/form.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  templateUrl: './form.component.html'
 })
 export class FormComponent implements OnInit {
 
