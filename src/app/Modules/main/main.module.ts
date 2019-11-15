7a38252f483006a9cd1f4fc0305e4cf75fe47f27
Michael McCarty <mmccarty@sgintl.com>
2019-11-15 10:43:11 -0500

    Added a form to dash component to start testing rxjs/ngrx

--- src/app/Modules/main/main.module.ts
+++ src/app/Modules/main/main.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashComponent } from './dash/dash.component';
+import { FormComponent } from './form/form.component';
 
 
 
 @NgModule({
-  declarations: [DashComponent],
+  declarations: [DashComponent, FormComponent],
   imports: [
     CommonModule
   ],
