aff4166305c44618496388bd24bcc3e2f29ddd82
Michael McCarty <mmccarty@sgintl.com>
2019-11-18 11:09:39 -0500

    Wiring data together

--- src/app/Modules/main/dash/dash.component.ts
+++ src/app/Modules/main/dash/dash.component.ts
@@ -2,9 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dash',
-  templateUrl: './dash.component.html'
+  templateUrl: './dash.component.html',
+  styleUrls: ['./dash.component.css']
 })
 export class DashComponent implements OnInit {
+
+  data=['data1','data2'];
 
   constructor() { }
 
--- src/app/Modules/main/units/units.component.ts
+++ src/app/Modules/main/units/units.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-units',
   templateUrl: './units.component.html'
 })
 export class UnitsComponent implements OnInit {
+
+  @Input() units: string[];
 
   constructor() { }
 
