595067d5614bb8036662d48076a8c535634e4f80
Michael McCarty <mmccarty@sgintl.com>
2019-11-25 17:15:55 -0500

    OCD commit for me

--- src/app/Modules/Auth/auth.service.ts
+++ src/app/Modules/Auth/auth.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../../Models/user.model';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { nextTick } from 'q';
 
 @Injectable({
   providedIn: 'root'
