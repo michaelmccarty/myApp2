31acb3e59c63e6fa379f7fa6c66cdffcf2235417
Michael McCarty <mmccarty@sgintl.com>
2019-12-02 17:34:51 -0500

    Tracking chat component.

---
+++ src/app/Modules/Main/chat/chat.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html'
+})
+export class ChatComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
