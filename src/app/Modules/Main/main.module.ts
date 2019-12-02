6d4cc1c0fc554f746a69d60ebaa1609d95a93894
Michael McCarty <mmccarty@sgintl.com>
2019-12-02 10:02:15 -0500

    Adding navigation bar with logout button

--- src/app/Modules/Main/main.module.ts
+++ src/app/Modules/Main/main.module.ts
@@ -6,9 +6,10 @@ import { UnitComponent } from './unit/unit.component';
 import { UnitsComponent } from './units/units.component';
 import { DataFormComponent } from './data-form/data-form.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [DashComponent, UnitComponent, UnitsComponent, DataFormComponent],
+  declarations: [DashComponent, UnitComponent, UnitsComponent, DataFormComponent, NavComponent],
   imports: [CommonModule, RouterModule, ReactiveFormsModule, FormsModule],
   exports: [DashComponent]
 })
---
+++ src/app/Modules/Main/nav/nav.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html'
+})
+export class NavComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
