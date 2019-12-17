1cffc1b05924f29416fe89c2f7e307f99b6c7501
michaelmccarty <mccartyatl@gmail.com>
2019-12-17 00:42:29 -0500

    Ship to work

--- server.js
+++ server.js
@@ -41,6 +41,10 @@ io.on('connection', socket => {
   socket.on('message', data => {
     console.log(data+" has been sent from "+socket.id);
   });
+
+  socket.on("disconnect", data =>{
+    console.log(socket.id+" has disconnected.");
+  })
 });
 
 // Start the server
