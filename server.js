238e657e56ce1c747b3cadbdd8aee721a5e8804f
Michael McCarty <mmccarty@sgintl.com>
2020-01-07 15:55:05 -0500

    Fixed ALL errors.

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(express.static('dist'));
 
 require('./auth')(app);
-require('./chat')(app)(io, chatMessages);
+require('./chat')(app, io, chatMessages);
 
 // Start the server
 server.listen(PORT, function() {
