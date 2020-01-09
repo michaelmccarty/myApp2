2387cf5c2dd4fcfaab8fc20cf46397744fd2d6e4
Michael McCarty <mmccarty@sgintl.com>
2020-01-09 15:34:00 -0500

    User actions typed up

--- src/app/store/user/user.actions.ts
+++ src/app/store/user/user.actions.ts
@@ -1,23 +1,30 @@
 import { Action } from '@ngrx/store';
 
-export const SEND_MESSAGE_SUCCESS = 'SEND_MESSAGE_SUCCESS';
-export const SEND_MESSAGE_FAILURE = 'SEND_MESSAGE_FAILURE';
+export const LOGIN = 'LOGIN';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 
 
-export class SendMessageSuccessAction implements Action {
-  readonly type = SEND_MESSAGE_SUCCESS;
-
-  constructor(public payload: any) {}
-}
-
-
-export class SendMessageFailureAction implements Action {
-  readonly type = SEND_MESSAGE_FAILURE;
+export class LoginAction implements Action {
+  readonly type = LOGIN;
 
   constructor(public payload: any) {}
 }
 
-export type All = SendMessageSuccessAction | SendMessageFailureAction;
+
+export class LoginSuccessAction implements Action {
+  readonly type = LOGIN_SUCCESS;
+
+  constructor(public payload: any) {}
+}
+
+export class LoginFailureAction implements Action {
+  readonly type = LOGIN_FAILURE;
+
+  constructor(public payload: any) {}
+}
+
+export type All = LoginAction | LoginSuccessAction | LoginFailureAction;
 // | LogOutAction
 // | LogInSuccessAction
 // | LogInFailureAction
